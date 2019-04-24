7821384ea8419c470abc6b5291268b385476f83d
CONWAY <kable2580@gmail.com>
2019-04-24 23:57:04 +0800

    refractor

--- app/src/main/java/twb/brianlu/com/firebasetest/ChatMessage.java
+++ app/src/main/java/twb/brianlu/com/firebasetest/ChatMessage.java
@@ -4,44 +4,41 @@ import java.util.Date;
 
 public class ChatMessage {
 
-    private String messageText;
-    private String messageUser;
-    private long messageTime;
+  private String messageText;
+  private String messageUser;
+  private long messageTime;
 
-    public ChatMessage(String messageText, String messageUser) {
-        this.messageText = messageText;
-        this.messageUser = messageUser;
+  public ChatMessage(String messageText, String messageUser) {
+    this.messageText = messageText;
+    this.messageUser = messageUser;
 
-        // Initialize to current time
-        messageTime = new Date().getTime();
-    }
+    // Initialize to current time
+    messageTime = new Date().getTime();
+  }
 
-    public ChatMessage(){
+  public ChatMessage() {}
 
-    }
+  public String getMessageText() {
+    return messageText;
+  }
 
-    public String getMessageText() {
-        return messageText;
-    }
+  public void setMessageText(String messageText) {
+    this.messageText = messageText;
+  }
 
-    public void setMessageText(String messageText) {
-        this.messageText = messageText;
-    }
+  public String getMessageUser() {
+    return messageUser;
+  }
 
-    public String getMessageUser() {
-        return messageUser;
-    }
+  public void setMessageUser(String messageUser) {
+    this.messageUser = messageUser;
+  }
 
-    public void setMessageUser(String messageUser) {
-        this.messageUser = messageUser;
-    }
+  public long getMessageTime() {
+    return messageTime;
+  }
 
-    public long getMessageTime() {
-        return messageTime;
-    }
-
-    public void setMessageTime(long messageTime) {
-        this.messageTime = messageTime;
-    }
+  public void setMessageTime(long messageTime) {
+    this.messageTime = messageTime;
+  }
 }
-
