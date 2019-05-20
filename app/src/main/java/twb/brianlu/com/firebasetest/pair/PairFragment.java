d8d49427e59771701f01834363b3de1de81cc7a2
brian <eebrian123tw93@gmail.com>
2019-05-21 01:58:08 +0800

    新增Pair UI 動畫

--- app/src/main/java/twb/brianlu/com/firebasetest/pair/PairFragment.java
+++ app/src/main/java/twb/brianlu/com/firebasetest/pair/PairFragment.java
@@ -1,10 +1,12 @@
 package twb.brianlu.com.firebasetest.pair;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -12,15 +14,36 @@ import ch.halcyon.squareprogressbar.SquareProgressBar;
 import twb.brianlu.com.firebasetest.R;
 
 
-public class PairFragment extends Fragment {
-
+public class PairFragment extends Fragment implements PairView {
+     SquareProgressBar squareProgressBar;
+    @SuppressLint("ClickableViewAccessibility")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_pair, container, false);
-        final SquareProgressBar squareProgressBar = view.findViewById(R.id.sprogressbar);
-        squareProgressBar.setImage(R.drawable.make_friends_color);
+        squareProgressBar = view.findViewById(R.id.sprogressbar);
+        squareProgressBar.setImage(R.drawable.make_friends);
+        squareProgressBar.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                switch (event.getAction()) {//当前状态
+                    case MotionEvent.ACTION_DOWN:
+                        squareProgressBar.setImage(R.drawable.make_friends_color);
+                        break;
+                    case MotionEvent.ACTION_MOVE:
+                        break;
+                    case MotionEvent.ACTION_UP:
+                        squareProgressBar.setImage(R.drawable.make_friends);
+                        break;
+                    default:
+                        break;
+                }
+                return true;
+
+            }
+        });
+
 
 //        squareProgressBar.setProgress(50);
         squareProgressBar.animate().start();
@@ -29,8 +52,8 @@ public class PairFragment extends Fragment {
             public void run() {
                 super.run();
                 for (int i = 0; true; i++) {
-                    if(i%102==0){
-                        i=0;
+                    if (i % 102 == 0) {
+                        i = 0;
                     }
                     squareProgressBar.setProgress(i);
                     try {
@@ -40,8 +63,46 @@ public class PairFragment extends Fragment {
                     }
                 }
             }
-        }.start();
+        };
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                for (int i = 0; true; i++) {
+                    if (i % 3 == 1) squareProgressBar.setImage(R.drawable.make_friends);
+                    else if (i % 3 == 2) squareProgressBar.setImage(R.drawable.make_friends_half);
+                    else squareProgressBar.setImage(R.drawable.make_friends_color);
+                    try {
+                        Thread.sleep(700);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        };
         squareProgressBar.setRoundedCorners(true);
         return view;
     }
+
+
+    @Override
+    public void onSetProgressBarImage(int image) {
+        squareProgressBar.setImage(image);
+    }
+
+    @Override
+    public void onSetProgressBarProgress(double value) {
+        squareProgressBar.setProgress(value);
+    }
+
+    @Override
+    public void onSetProgressBarVisibility(boolean visible) {
+        
+    }
+
+    @Override
+    public void onSetMessage(String message, int type) {
+
+    }
+
 }
--- app/src/main/java/twb/brianlu/com/firebasetest/pair/PairView.java
+++ app/src/main/java/twb/brianlu/com/firebasetest/pair/PairView.java
@@ -3,4 +3,9 @@ package twb.brianlu.com.firebasetest.pair;
 import twb.brianlu.com.firebasetest.core.BaseView;
 
 public interface PairView extends BaseView {
+    void onSetProgressBarVisibility(boolean visible);
+
+    void onSetProgressBarImage(int image);
+
+    void onSetProgressBarProgress(double value);
 }
