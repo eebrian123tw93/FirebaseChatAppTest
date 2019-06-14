88041b1557e430a94ae3319065caecc7093c001e
CONWAY <kable2580@gmail.com>
2019-06-14 17:51:51 +0800

    send photo 50% done

--- app/src/main/java/twb/brianlu/com/firebasetest/model/ChatMessage.java
+++ app/src/main/java/twb/brianlu/com/firebasetest/model/ChatMessage.java
@@ -2,6 +2,7 @@ package twb.brianlu.com.firebasetest.model;
 
 import java.util.Date;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
@@ -11,13 +12,15 @@ public class ChatMessage {
   private String userUid;
   private String messageUser;
   private long messageTime;
+  private FileModel fileModel;
+
+  public ChatMessage() {}
 
   public ChatMessage(String messageText, String messageUser, String userUid) {
     this.messageText = messageText;
     this.messageUser = messageUser;
     messageTime = new Date().getTime();
     this.userUid = userUid;
+    fileModel = null;
   }
-
-  public ChatMessage() {}
 }
---
+++ app/src/main/java/twb/brianlu/com/firebasetest/model/FileModel.java
@@ -0,0 +1,10 @@
+package twb.brianlu.com.firebasetest.model;
+
+import lombok.Data;
+
+@Data
+public class FileModel {
+  private String type;
+  private String fileUrl;
+  private String fileName;
+}
